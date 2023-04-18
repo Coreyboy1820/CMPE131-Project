7c799501e77e2bab6b53bb38677bcc32a41ccec3
Corey Kelley <coreyboy1820@hotmail.com>
2023-04-17 21:01:40 -0700

    ✨ Create templates for all endpoints along with a basic navbar at the top of each one

---
+++ app/routes.py
@@ -0,0 +1,27 @@
+from app import myapp_obj
+from flask import render_template, redirect, url_for
+
+@myapp_obj.route("/", methods=["GET"])
+def to_home():
+    return redirect(url_for('home'))
+
+@myapp_obj.route("/home")
+def home():
+    return render_template(
+    'home.html', title="home", user='Corey')
+
+@myapp_obj.route("/login")
+def login():
+    return render_template('todo.html', title="home", user='Corey')
+
+@myapp_obj.route("/register")
+def register():
+    return render_template('register.html', title="home", user='Corey')
+
+@myapp_obj.route("/todo")
+def todo():
+    return render_template('emails.html', title="home", user='Corey')
+
+@myapp_obj.route("/emails")
+def emails():
+    return render_template('login.html', title="home", user='Corey')
