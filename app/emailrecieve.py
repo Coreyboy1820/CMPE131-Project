0a94403cc2f1b8d10ac8414341db412917aa00a6
Labibeh Taghizadeh <labibeh.taghizadeh@sjsu.edu>
2023-04-28 10:48:50 -0700

    test

--- app/emailrecieve.py
+++ app/emailrecieve.py
@@ -2,15 +2,11 @@ from app import db, Session, message, recipient, user
 
 # First, let's check if the recipient exists in the database
 session = Session()
-recipient = session.query(user).filter_by(id=recipient_id).first()
-if recipient is None:
-    print(f"No recipient with id {recipient_id} found in the database.")
-else:
-    # If the recipient exists, let's retrieve all the messages addressed to them
-    messages = session.query(message).join(recipient).filter(recipient.id == recipient_id).all()
-    if not messages:
-        print(f"No messages found for recipient with id {recipient_id}.")
-    else:
+
+    messages = session.query(message).all()
+    for message in messages:
+        if  messages.recipient.userId==user
+        
         # Display the messages on the recipient's page
         print(f"Messages for recipient with id {recipient_id}:")
         for msg in messages:
@@ -21,9 +17,8 @@ else:
 
         # Define the relationship between the recipient and the messages in the database
         for msg in messages:
-            rec = recipient(user_id=recipient_id, message_id=msg.id)
+            rec = recipient(userId=recipient_id, message_id=msg.id)
             session.add(rec)
 
         # Commit the changes to the database
         session.commit()
-
--- app/forms/contactForm.py
+++ app/forms/contactForm.py
@@ -1,10 +1,25 @@
-from django import forms
+
+from flask_wtf import FlaskForm
+from wtforms import charField, StringField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email
+from app import models
+from flask import flash
   
 # creating a form
 class InputForm(forms.Form):
-  
+ 
+    Email = StringField('Email', validators=[DataRequired(), Email()])
     Email= forms.CharField(max_length = 200)
     ContactId = forms.CharField(max_length = 200)
     Nickname = forms.CharField(max_length = 200)
   
- 
+  submit = SubmitField('AddContact')
+
+class addContact():
+def validate(userEmail):
+          dbSession = models.Session()
+          # check if the userEmail existed in the database
+          if (dbSession.query(models.user).filter_by(email=userEmail).first() is None):
+               return True
+          else:
+                  flash('Invalid Email')
