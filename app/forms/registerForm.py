6c95ba3a79655cd02cd0d6cbd8066001185f88a5
Labibeh Taghizadeh <labibeh.taghizadeh@sjsu.edu>
2023-05-09 23:02:41 -0700

    2 password requirments

--- app/forms/registerForm.py
+++ app/forms/registerForm.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms.validators import DataRequired,  Length, Regexp, EqualTo, Email
 from app import models
 from flask import flash
 
@@ -8,11 +8,11 @@ class RegisterForm(FlaskForm):
       email = StringField('Email', validators=[DataRequired(), Email()])  
 
       # the text input that takes password input from user. Required
-      password = PasswordField('Password', validators=[DataRequired()])
+      password = PasswordField('Password', validators=[DataRequired(),  Length(min=1, max=8),  Regexp(r'.*[0-9].*', message='Password must contain at least one digit between 0-9')])
 
       # the text input that allows user to type their password again. Required. Input must be the same as the input of password
       confirmPassword = PasswordField('Confirm Password', validators=[
-        DataRequired(), EqualTo('password', message='Passwords must match')])
+        DataRequired(), Length(min=1, max=8), EqualTo('password', message='Passwords must match')])
       
       # submit button
       submit = SubmitField('Register')
@@ -22,11 +22,16 @@ class RegisterFunction():
       def validate(userEmail, userPassword, userConfirmPassword):
           dbSession = models.Session()
           # check if the userEmail existed in the database
-          if (dbSession.query(models.user).filter_by(email=userEmail).first() is None and userPassword == userConfirmPassword):
-               return True
-          if (dbSession.query(models.user).filter_by(email=userEmail).first() is not None):
-               flash('Email is already registered', category='error')
-          if (userPassword != userConfirmPassword):
-               flash('The password confirmation does not match', category='error')
-          return False
-               
+          if dbSession.query(models.user).filter_by(email=userEmail).first() is not None:
+              flash('Email is already registered', category='error')
+              return False
+
+          if userPassword != userConfirmPassword:
+              flash('The password confirmation does not match', category='error')
+              return False
+
+          if len(userPassword) > 8:
+              flash('The password must not exceed 8 characters', category='error')
+              return False
+
+          return True
